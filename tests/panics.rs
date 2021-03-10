5dff90478a7ed1528a102bd8485058c5631fe659
Tilman Roeder <dyed.green.info@gmail.com>
2021-03-10 14:42:23 +0000

    Start on docs

--- tests/panics.rs
+++ tests/panics.rs
@@ -1,16 +1,22 @@
-use rand_distr::Bernoulli;
-use uncertain::{Distribution, Uncertain};
+use uncertain::{PointMass, Uncertain};
 
 #[test]
 #[should_panic]
 fn test_too_large_pr_panics() {
-    let x = Distribution::from(Bernoulli::new(0.5).unwrap());
+    let x = PointMass::new(true);
     x.pr(1.2);
 }
 
 #[test]
 #[should_panic]
 fn test_negative_pr_panics() {
-    let x = Distribution::from(Bernoulli::new(0.5).unwrap());
+    let x = PointMass::new(true);
     x.pr(-0.3);
+}
+
+#[test]
+#[should_panic]
+fn test_nagative_expect_precision_panics() {
+    let x = PointMass::new(0.0);
+    x.expect(-0.1);
 }
